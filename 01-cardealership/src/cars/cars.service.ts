751b286888adaf278c4d13a3075f7912b38d989c
Tou-u <rodrignacio99@hotmail.com>
2024-01-05 13:17:38 -0300

    Curso de autos

---
+++ 01-cardealership/src/cars/cars.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
+import { CreateCarDto, UpdateCarDto } from './dto';
+
+@Injectable()
+export class CarsService {
+  private cars: Car[] = [];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: string) {
+    const car = this.cars.find((car) => car.id === id);
+
+    if (!car) throw new NotFoundException(`Car with id ${id} not found`);
+
+    return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+    this.cars.push(car);
+    return car;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto) {
+    let carDB = this.findOneById(id);
+
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        carDB = {
+          ...carDB,
+          ...updateCarDto,
+          id,
+        };
+        return carDB;
+      }
+
+      return car;
+    });
+
+    return carDB;
+  }
+
+  delete(id: string) {
+    this.findOneById(id);
+    this.cars = this.cars.filter((c) => c.id !== id);
+  }
+
+  fillCarsWithSeedData(cars: Car[]) {
+    this.cars = cars;
+  }
+}
