b7daa59bb4bf33afd7d4e5e233a8dd50167853da
Tou-u <rodrignacio99@hotmail.com>
2024-01-07 19:09:27 -0300

    ejemplo de documentación con swagger

--- 03-ecommerce/src/products/entities/product.entity.ts
+++ 03-ecommerce/src/products/entities/product.entity.ts
@@ -9,51 +9,89 @@ import {
 } from 'typeorm';
 import { ProductImage } from './product-image.entity';
 import { User } from 'src/auth/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty({
+    example: '0289472e-d022-4718-a5db-6ee82418a7a1',
+    description: 'Product id',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'T-Shirt Teslo',
+    description: 'Product title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({
+    example: 0,
+    description: 'Product price',
+  })
   @Column('float', {
     default: 0,
   })
   price: number;
 
+  @ApiProperty({
+    example: 'In non mollit irure irure magna esse.',
+    description: 'Product description',
+    default: null,
+    required: false,
+  })
   @Column('text', {
     nullable: true,
   })
   description: string;
 
+  @ApiProperty({
+    example: '/t-shirt_teslo',
+    description: 'Product Slug for SEO',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   slug: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Product stock',
+    default: 0,
+  })
   @Column('int', {
     default: 0,
   })
   stock: number;
 
+  @ApiProperty({
+    example: ['M', 'XL'],
+    description: 'Product sizes',
+  })
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
+  @ApiProperty()
   @Column('text')
   gender: string;
 
+  @ApiProperty()
   @Column('text', {
     array: true,
     default: [],
   })
   tags: string[];
 
+  @ApiProperty()
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
